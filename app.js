e4c52d5ec0707a0985373058590a21bb5e730ba8
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2022-07-19 21:11:23 -0300

    base proyect for sprint 3 with ejs example

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require('express'); 
 const path =  require('path');
 const app = express(); 
+const mainRouter = require('./src/routes/mainRouter');
 
 
-const viewsPath = path.join(__dirname, 'views');
+const viewsPath = path.join(__dirname, '/src/views/users/');
 const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join( viewsPath, 'home.html'));
-});
-
-app.get('/carrito', (req, res) => res.sendFile(path.join(viewsPath, 'carrito.html')));
-
-app.get('/detalle', (req, res) => res.sendFile(path.join(viewsPath, 'detalle.html')));
-
-app.get('/loggin', (req, res) => res.sendFile(path.join(viewsPath, 'loggin.html')));
-
-app.get('/registro', (req, res) => res.sendFile(path.join(viewsPath, 'registro.html')));
+app.set('view engine', 'ejs');
+app.set('views', viewsPath);
+app.use('/', mainRouter);
 
 app.listen (process.env.PORT || 3001, ()=>{
     console.log('Servidor funcionando bien');
---
+++ src/controllers/mainController.js
@@ -0,0 +1,14 @@
+const plantas = require('../database/plantas.js');
+
+
+const mainController = {
+    index: function( req, res){
+        res.render('home', {
+            plantas: plantas,
+        })
+    }
+
+};
+
+
+module.exports = mainController;
