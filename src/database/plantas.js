68cd0d35fe5400115bec2080fbf5d927305a7159
Andrenegrip <andrenegrip@gmail.com>
2022-07-23 14:54:35 -0300

    database

---
+++ src/database/plantas.js
@@ -0,0 +1,40 @@
+const plantas = [
+    {
+        id: 1,
+        name: "Limón 4 estaciones",
+        price: "$2.500",
+        image: "/images/limon.jpg"
+    },
+    {
+        id: 2,
+        name: "Jazmin kimura",
+        price: "$1.500",
+        image: "/images/Jazmin-kimura.jpg"
+    },
+    {
+        id: 3,
+        name: "Pino",
+        price: "$700",
+        image: "/images/pino.jpg"     
+    },
+    {
+        id: 4,
+        name: "Lavanda dentada",
+        price: "$180",
+        image: "/images/lavanda.jpg"     
+    },   
+    {
+        id: 5,
+        name: "Flor de azúcar",
+        price: "$140",
+        image: "/images/flor-de-azucar.jpg"     
+    },     
+    {
+        id: 6,
+        name: "Erica carnea",
+        price: "$590",
+        image: "/images/erica-carnea.jpeg"   
+    }
+]
+
+module.exports = plantas;
